8ec16fbdebec13878838ae3956e04935ead63cc9
Wesley Silva <wjesus@2mundos.net>
2019-06-30 23:32:56 -0300

    adding logo, search and today component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import React, { Component } from "react";
+import "./assets/styles/index.scss";
+import Today from "./components/Today/Today";
 
 class App extends Component {
-
   render() {
     return (
       <div className="App">
+        <div className="ui container">
+          <div className="row">
+            <div className="logo">
+              {
+                <img
+                  src={require("./assets/logo-todo.png")}
+                  alt="Things to do in BRNO"
+                />
+              }
+            </div>
+          </div>
+
+          <div className="row">
+            <form className="search__form">
+              <div className="field">
+                <input type="text" name="search" placeholder="Search Todo" />
+              </div>
+            </form>
+          </div>
+
+          <div className="row">
+            <Today />
+          </div>
+        </div>
       </div>
     );
   }
